caddc406e711142ca513af9889ea313703fbcfd8
Первый комит <suzdalcev97t@gmail.com>
2025-04-29 23:51:37 +0400

    Готово

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,79 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import jdk.nio.mapmode.ExtendedMapMode;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+import java.util.Random;
+
+public class Main {
+
+    private final static Random RANDOM = new Random();
+    private final static String[] NAMES = {"Леонид", "Василий", "Петр", "Мирон", "Анатолий", "Семен", "Евгений"};
+    private final static String[] SURNAMES = {"Леонидов", "Васильев", "Петров", "Миронов", "Игнатов", "Семенов", "Попов"};
+    private final static String[] PATRONYMIC_NAMES = {"Леонидович", "Васильевич", "Петрович", "Миронович", "Анатольевич", "Семенович", "Евгеньевич"};
+
+    private final static Employee[] EMPLOYEES = new Employee[10];
+
+    private static void initEmployees() {
+        for (int i = 0; i < EMPLOYEES.length; i++) {
+            String fullName = SURNAMES[RANDOM.nextInt(0, SURNAMES.length)] + " " +
+                    NAMES[RANDOM.nextInt(0, NAMES.length)] + " " +
+                    PATRONYMIC_NAMES[RANDOM.nextInt(0, NAMES.length)] + " ";
+            EMPLOYEES[i] = new Employee(fullName, RANDOM.nextInt(1, 6), RANDOM.nextInt(50_000, 100_000));
+
+
+        }
+    }
+
+    public static void main(String[] args) {
+        initEmployees();
+        print();
+        System.out.println("Сумма зп сотрудников: " + calculeteSumOfSalarries());
+        System.out.println("Cотрудник с мин зп: " + findEmployeeWithMinSalaru());
+        System.out.println("Сотрудник с макс зп: " + findEmployeeWithMaxSalaru());
+        System.out.println("Средняя зп " + calculeteAveregOfSalarries());
+        printFullName();
+
+    }
+
+    private static void print() {
+        for (Employee employee : EMPLOYEES) {
+            System.out.println(employee);
+        }
+    }
+
+    private static int calculeteSumOfSalarries() {
+        int sum = 0;
+        for (Employee employee : EMPLOYEES) {
+            sum += employee.getSalary();
+        }
+        return sum;
+    }
+
+    private static Employee findEmployeeWithMinSalaru() {
+        Employee employeeWinMinSalaru = null;
+        for (Employee employee : EMPLOYEES) {
+            if (employeeWinMinSalaru == null || employee.getSalary() < employeeWinMinSalaru.getSalary()) {
+                employeeWinMinSalaru = employee;
+            }
+        }
+        return employeeWinMinSalaru;
+    }
+
+    private static Employee findEmployeeWithMaxSalaru() {
+        Employee employeeWinMaxSalaru = null;
+        for (Employee employee : EMPLOYEES) {
+            if (employeeWinMaxSalaru == null || employee.getSalary() > employeeWinMaxSalaru.getSalary()) {
+                employeeWinMaxSalaru = employee;
+            }
+        }
+        return employeeWinMaxSalaru;
+    }
+
+    private static double calculeteAveregOfSalarries() {
+        return (double) calculeteSumOfSalarries() / EMPLOYEES.length;
+    }
+
+    private static void printFullName() {
+        for (Employee employee : EMPLOYEES) {
+            System.out.println(employee.getFollName());
         }
     }
 }
